08e46f5023e921a1858e704866a3d7a096c51104
eduardolirainfo <eduardolirainfo@gmail.com>
2025-02-13 22:46:20 -0300

    Types and Fields

--- graphene_start/main.py
+++ graphene_start/main.py
@@ -1,19 +1,59 @@
-from graphene import ObjectType, Schema, String
+from graphene import Field, Int, ObjectType, Schema, String
+
+
+class UserType(ObjectType):
+    id = Int()
+    name = String()
+    age = Int()
+    email = String()
 
 
 class Query(ObjectType):
-    hello = String(name=String(default_value='mundo'))
+    user = Field(UserType, user_id=Int())
+    users = [
+        {
+            'id': 1,
+            'name': 'John',
+            'age': 35,
+            'email': 'demo1@gmail.com',
+        },
+        {
+            'id': 2,
+            'name': 'Jane',
+            'age': 28,
+            'email': 'demo2@gmail.com',
+        },
+        {
+            'id': 3,
+            'name': 'Alice',
+            'age': 32,
+            'email': 'demo3@gmail.com',
+        },
+        {
+            'id': 4,
+            'name': 'Bob',
+            'age': 45,
+            'email': 'demo4@gmail.com',
+        },
+    ]
 
     @staticmethod
-    def resolve_hello(parent, info, name):
-        return f'Olá {name}'
+    def resolve_user(parent, info, user_id):
+        matched_users = [user for user in Query.users if user['id'] == user_id]
+
+        return matched_users[0] if matched_users else None
 
 
 schema = Schema(query=Query)
 
 gql = """
-{
-  hello(name: "GraphQL")
+query{
+  user(userId: 3){
+    id
+    name
+    age
+    email
+  }
 }
 """
 
